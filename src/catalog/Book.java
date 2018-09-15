65b45aefe33174a0766cdfd75e1d26272500b487
Olivia Smith <31713886+liv-yaa@users.noreply.github.com>
2018-09-14 19:08:59 -0700

    Deleted comments etc

--- src/catalog/Book.java
+++ src/catalog/Book.java
@@ -3,9 +3,12 @@ package catalog;
 /** Class Book represents a book in the library catalog */
 public class Book {
 
-    // FILL IN CODE: declare private instance variables:
-    // title, author, year, averageRating, isCheckedOut
-
+    // FILL IN CODE: declare private instance variables: title, author, year, averageRating, isCheckedOut:
+    private String title;
+    private String author;
+    private int year;
+    private double averageRating;
+    private boolean isCheckedOut;
 
     /**
      * Constructor of class Book
@@ -14,21 +17,22 @@ public class Book {
      * @param year year when the book was first published
      * @param avgRating average rating on goodreads
      */
-    public Book(String title, String author, int year,
-                double avgRating){
-        // FILL IN CODE
+    public Book(String title, String author, int year, double avgRating) {
 
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.averageRating = avgRating;
+        isCheckedOut = false;
     }
 
     /**
-     * Return true if the book is currently checked out
-     * and false otherwise.
+     * Return true if the book is currently checked out and false otherwise.
      * @return value of boolean variable isCheckedOut
      */
     public boolean isCheckedOut() {
-        // FILL IN CODE
-
-        return false; // remember to change it!
+        // FILL IN CODE - this is sufficient to return true / false (since the value was initialized in the constructor
+        return isCheckedOut;
     }
 
     /**
@@ -37,51 +41,48 @@ public class Book {
      */
     public String getTitle() {
         // FILL IN CODE
-
-        return ""; // remember to change it
+        return title;
     }
 
     /** Return average rating of the book from goodreads
-     *
-     * @return average rating
-     */
+     * @return average rating */
     public double getAverageRating() {
         // FILL IN CODE
-
-        return 0; // remember to change it
+        return averageRating;
     }
 
     /**
-     * Check out the book if it is available,
-     * by updating isCheckedOut variable.
+     * Check out the book if it is available by updating isCheckedOut variable.
      * If the book is not available (checked out), return false
      * @return true if was able to checkout the book, and false otherwise
      */
     public boolean checkoutBook() {
         // FILL IN CODE
 
-        return false; // remember to change it
+        if (isCheckedOut == false) {
+            isCheckedOut = true;
+            return true;
+        }
+        return false;
     }
 
-    /**
-     * Return the book if it is currently checked out, return true.
-     * If the book is not checked out (can not return), return false
-     * @return true if was able to return a book, false otherwise
-     */
+    /** Return the book if it is currently checked out, return true. If the book is not checked out (can not return), return false
+     * @return true if was able to return a book, false otherwise */
+
     public boolean returnBook() {
         // FILL IN CODE
-
-        return false; // remember to change it
+        if (isCheckedOut == true) {
+            isCheckedOut = false;
+            return true;
+        }
+        return false;
     }
 
-    /** Return a string with the information about the book
-     * in the following format:
-     * (title, author, year, averageRating)
-     * @return string representation of the book
-     */
+    /** Return a string with the information about the book in the following format: (title, author, year, averageRating)
+     * @return string representation of the book */
+
     public String toString() {
         // FILL IN CODE
-
-        return ""; // // remember to change it
+        return "(" + title + ", " + author + ", " + year + ", " + averageRating + ")";
     }
 }
