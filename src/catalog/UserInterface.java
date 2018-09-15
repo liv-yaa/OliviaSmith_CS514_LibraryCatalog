cdf8204963d6246bf89b80a97196db9df8a016d7
Olivia Smith <31713886+liv-yaa@users.noreply.github.com>
2018-09-14 19:11:25 -0700

    Better UI

--- src/catalog/UserInterface.java
+++ src/catalog/UserInterface.java
@@ -1,6 +1,6 @@
 package catalog;
 
-import java.util.Scanner;
+import java.util.*;
 
 /** This class allows the user to interact with the system
  * using keyboard input.
@@ -12,30 +12,97 @@ public class UserInterface {
     public final static String EXIT  = "4";
 
     /**
-     * The method interacts with the user allowing them
-     * to checkout a book, to return a book, to see a list
+     * The method interacts with the user allowing them to checkout a book, to return a book, to see a list
      * of available books, and to exit the program.
      * @param lc Library Catalog
+     *
+     *         // Repeatedly prompt the user:
+     *         // To check out a book, enter 1
+     *         // To return a book, enter 2
+     *         // To , enter 3
+     *         // To exit, enter 4
      */
     public void mainLoop(LibraryCatalog lc) {
         // FILL IN CODE
-        // Repeatedly prompt the user:
-        // To check out a book, enter 1
-        // To return a book, enter 2
-        // To see a list of books currently available, enter 3
-        // To exit, enter 4
 
-        // If the user enters 1, ask for the title, and if the book is available,
-        // check it out
+        Scanner sc = new Scanner(System.in);
+        
+        String inputInt; // Using constants -
 
-        // If the user enters 2, ask for the title, and if the book is currently
-        // checked out, return it.
+        do {
+            System.out.println("Welcome to the Library Catalog!");
+            System.out.println();
+            System.out.println("- To check out a book, enter 1.");
+            System.out.println("- To return a book, enter 2.");
+            System.out.println("- To see a list of books currently available, enter 3.");
+            System.out.println("- To exit, enter 4.");
+            System.out.println();
 
-        // If a user enters 3, display all the books that are currently
-        // available (not checked out).
+            inputInt = sc.next();
 
-        // If a user enters 4, exit the program
+            // If the user enters 1, ask for the title, and if the book is available, check it out
+            if (inputInt.matches(CHECKOUT)) {
 
+                boolean valid = true; // Checks that the user input (1-4) is valid.
 
-    } // mainLoop
+                do {
+                    System.out.println("What is the title to check out? Or to go back to main menu, enter 0.");
+                    String title = sc.next();
+
+                    if (title.matches("0")) {
+                        valid = false;
+                        break; // Go back to main menu
+                    }
+
+                    if (!(lc.checkoutBook(title))) { // Try to check out. If can't, prompts user to try again.
+                        System.out.println("Book cannot be checked out. Please try again: ");
+                        continue;
+                    }
+                    else {
+                        valid = false;
+                        System.out.println(title + " has been checked out. All Books (Updated): ");
+                        System.out.println(lc.getAvailableBooks());
+                    }
+                } while (valid);
+
+            }
+
+            // If the user enters 2, ask for the title, and if the book is currently checked out, return it.
+            else if (inputInt.matches(RETURN)) {
+
+                boolean valid = true;
+
+                do {
+                    System.out.println("What is the title to return? Or to go back to main menu, enter 0.");
+                    String title = sc.next();
+
+                    if (title.matches("0")) {
+                        valid = false;
+                        break; // Go back to main menu
+                    }
+
+                    if (!(lc.returnBook(title))) {
+                        System.out.println("Book cannot be returned. Please try again: ");
+                        continue;
+                    }
+
+                    else {
+                        valid = false;
+                        System.out.println(title + " has been returned. All Books (Updated): ");
+                        System.out.println(lc.getAvailableBooks());
+                    }
+                }
+                while (valid);
+            }
+            // If a user enters 3, display all the books that are currently available (not checked out).
+            else if (inputInt.matches(PRINT_AVAILABLE)) {
+
+                System.out.println("All available books: ");
+                System.out.println(lc.getAvailableBooks());
+            }
+        }
+        while (!(inputInt.matches(EXIT))) ;
+        System.out.println("Goodbye!"); // If a user enters 4, exit the program
+
+    }
 }
